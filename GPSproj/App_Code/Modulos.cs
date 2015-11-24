96d5a55834352477609ce2c8bf0afc4fd9661b1f
glclopes <always_metal@yahoo.com>
2015-11-24 15:43:10 +0000

    actualizado

--- GPSproj/App_Code/Modulos.cs
+++ GPSproj/App_Code/Modulos.cs
@@ -6,20 +6,43 @@ using System.Web;
 /// <summary>
 /// Classe com os métodos responsáveis pelas operações de cálculo de X de uma equação do tipo ax^2+bx+c=0 .
 /// </summary>
+/// 
+
 public class Calculo
 {
-    //Calculo x1 pela formula resolvente quando existem os 3 termos a, b e c.
+    //Calcula x1 pela formula resolvente quando existem os 3 termos a, b e c.
     //Recebe a, b, e c e retorna o resultado.
     public double FR_a_x1(double a, double b, double c)
     {
         return (-b + Math.Sqrt(b - 4 * a * c)) / (a * c);
     }
 
-    //Calculo x2 pela formula resolvente quando existem os 3 termos a, b e c
+    //Calcula x2 pela formula resolvente quando existem os 3 termos a, b e c
     //Recebe a, b, e c e retorna o resultado.
     public double FR_a_x2(double a, double b, double c)
     {
         return (-b - Math.Sqrt(b - 4 * a * c)) / (a * c);
+    }
+
+    //Calcula X quando o termo a=0.
+    //Recebe b e c e retorna o resultado.
+    public double FR_b(double a, double b, double c)
+    {
+        return 2.3;
+    }
+
+    //Calcula x1 quando o termo b=0.
+    //Recebe a e c e retorna o resultado.
+    public double FR_c_x1(double a, double b, double c)
+    {
+        return 2.3;
+    }
+
+    //Calcula x2 quando o termo b=0.
+    //Recebe a e c e retorna o resultado.
+    public double FR_c_x2(double a, double b, double c)
+    {
+        return 2.3;
     }
 
 }
