6cd05b70ba068b537d5aba66a4d4e1b1ffc40fa2
pedroafcamacho <pedroafcamacho@gmail.com>
2015-12-01 17:15:16 +0000

    Update Testes.cs
    Exemplo de teste

--- GPSproj/Testes/Testes.cs
+++ GPSproj/Testes/Testes.cs
@@ -14,5 +14,14 @@ namespace Testes
             int sum = teste.testaSoma(1, 2);
             Assert.AreEqual(3, sum);
         }
+        public void TestPressCalcular()
+        {
+        Copias teste = new Copias();
+        calculator.Enter(2m);
+        calculator.PressPlus();
+        calculator.Enter(2m);
+        calculator.PressEquals();
+        Assert.AreEqual(4m, calculator.Display);
+        }
     }
 }
